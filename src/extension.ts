e667e683c6c88a35d4a5abc6706f63a54a1369d3
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-11-15 18:45:36 +0200

    refactor code to follow command pattern

--- src/extension.ts
+++ src/extension.ts
@@ -4,21 +4,32 @@ import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+type Framework = "nextjs" | "sveltekit";
+type CommandName = "search" | "rename" | "create";
+interface Command {
+  commandId: `project-explorer.${Framework}.${string}`;
+  register(context: vscode.ExtensionContext, cb: () => void): void;
+}
+class SearchFileCommand implements Command {
+  commandId: `project-explorer.${Framework}.${CommandName}`;
+  constructor(commandId: `project-explorer.${Framework}.${CommandName}`) {
+    this.commandId = commandId;
+  }
+  register(context: vscode.ExtensionContext, cb: () => void): void {
+    const disposable = vscode.commands.registerCommand(this.commandId, cb);
+    context.subscriptions.push(disposable);
+  }
+}
+export function activate(_: vscode.ExtensionContext) {
   console.log(
     'Congratulations, your extension "project-explorer" is now active!'
   );
-
-  let disposable = vscode.commands.registerCommand(
-    "project-explorer.hello",
-    () => {
-      vscode.window.showInformationMessage(
-        "Hello World from Project Explorer!"
-      );
-    }
+  const searchCommand: SearchFileCommand = new SearchFileCommand(
+    "project-explorer.nextjs.search"
   );
-
-  context.subscriptions.push(disposable);
+  searchCommand.register(_, () => {
+    vscode.window.showInformationMessage("Hello World from Project Explorer!");
+  });
 }
 
 // This method is called when your extension is deactivated
