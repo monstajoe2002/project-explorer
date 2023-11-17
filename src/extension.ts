19e2c743a9b86b1aade76b023cdc6f8b4837eb82
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-11-18 00:01:30 +0200

    Refactor file selection logic in activate function

--- src/extension.ts
+++ src/extension.ts
@@ -33,93 +33,96 @@ class SearchFileCommand implements Command {
 const uri = vscode.workspace.workspaceFolders![0].uri;
 const workspacePath = uri.fsPath;
 
-const searchCommand: SearchFileCommand = new SearchFileCommand("nextjs");
-
 export function activate(_: vscode.ExtensionContext) {
+  const { framework, isValidNextFile, register } = new SearchFileCommand(
+    "nextjs"
+  );
+
   if (!vscode.workspace.workspaceFolders) {
     vscode.window.showInformationMessage("No workspace folder open");
     return;
   }
-  searchCommand.register(_, async () => {
-    const { framework } = searchCommand;
+  async function showDirectoryContents(uri: vscode.Uri) {
+    const filesAndFolders = await vscode.workspace.fs.readDirectory(uri);
+    const folders = filesAndFolders
+      .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
+      .map(([name, fileType]) => ({
+        name,
+        fileType,
+        label: `${name}/`,
+        description: "Folder",
+      }));
+    const files = filesAndFolders
+      .filter(
+        ([name, fileType]) =>
+          fileType === vscode.FileType.File && isValidNextFile(name)
+      )
+      .map(([name, fileType]) => ({
+        name,
+        fileType,
+        label: "/",
+        description: name.includes("page") ? "page" : "layout",
+      }));
+    const selectedItem = await vscode.window.showQuickPick([
+      ...files,
+      ...folders,
+    ]);
+    if (selectedItem) {
+      const selectedUri = vscode.Uri.joinPath(uri, selectedItem.name);
+      if (selectedItem.fileType === vscode.FileType.Directory) {
+        await showDirectoryContents(selectedUri);
+      } else {
+        const selectedFile = await vscode.workspace.openTextDocument(
+          selectedUri
+        );
+        await vscode.window.showTextDocument(selectedFile);
+      }
+    }
+  }
+  async function nextInit() {
+    const APP_DIR_PATH = `${workspacePath}/app`;
+    const appDirUri = vscode.Uri.file(APP_DIR_PATH);
+    const appDir = await vscode.workspace.fs.readDirectory(appDirUri);
+    if (!appDir) {
+      return;
+    }
+    const appDirFolders = appDir
+      .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
+      .map(([name, fileType]) => ({
+        name,
+        fileType,
+        label: `${name}/`,
+        description: "Folder",
+      }));
+    const appDirFiles = appDir
+      .filter(
+        ([name, fileType]) =>
+          fileType === vscode.FileType.File && isValidNextFile(name)
+      )
+      .map(([name, fileType]) => ({
+        name,
+        fileType,
+        label: "/",
+        description: name.includes("page") ? "page" : "layout",
+      }));
+    const selected = await vscode.window.showQuickPick([
+      ...appDirFiles,
+      ...appDirFolders,
+    ]);
+    if (!selected) {
+      return;
+    }
+    const selectedPath = APP_DIR_PATH.concat("/", selected.name);
+    const selectedUri = vscode.Uri.file(selectedPath);
+    showDirectoryContents(selectedUri);
+  }
+  register(_, async () => {
     switch (framework) {
       case "nextjs":
-        const APP_DIR_PATH = `${workspacePath}/app`;
-        const appDirUri = vscode.Uri.file(APP_DIR_PATH);
-        const appDir = await vscode.workspace.fs.readDirectory(appDirUri);
-        if (!appDir) {
-          return;
-        }
-        const appDirFolders = appDir
-          .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
-          .map(([name, fileType]) => ({
-            name,
-            fileType,
-            label: `${name}/`,
-            description: "Folder",
-          }));
-        const appDirFiles = appDir
-          .filter(
-            ([name, fileType]) =>
-              fileType === vscode.FileType.File &&
-              searchCommand.isValidNextFile(name)
-          )
-          .map(([name, fileType]) => ({
-            name,
-            fileType,
-            label: "/",
-            description: name.includes("page") ? "page" : "layout",
-          }));
-        let selected = await vscode.window.showQuickPick([
-          ...appDirFiles,
-          ...appDirFolders,
-        ]);
-        if (!selected) {
-          return;
-        }
-        async function showDirectoryContents(uri: vscode.Uri) {
-          const filesAndFolders = await vscode.workspace.fs.readDirectory(uri);
-          const folders = filesAndFolders
-            .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
-            .map(([name, fileType]) => ({
-              name,
-              fileType,
-              label: `${name}/`,
-              description: "Folder",
-            }));
-          const files = filesAndFolders
-            .filter(
-              ([name, fileType]) =>
-                fileType === vscode.FileType.File &&
-                searchCommand.isValidNextFile(name)
-            )
-            .map(([name, fileType]) => ({
-              name,
-              fileType,
-              label: "/",
-              description: name.includes("page") ? "page" : "layout",
-            }));
-          const selectedItem = await vscode.window.showQuickPick([
-            ...files,
-            ...folders,
-          ]);
-          if (selectedItem) {
-            const selectedUri = vscode.Uri.joinPath(uri, selectedItem.name);
-            if (selectedItem.fileType === vscode.FileType.Directory) {
-              await showDirectoryContents(selectedUri);
-            } else {
-              const selectedFile = await vscode.workspace.openTextDocument(
-                selectedUri
-              );
-              await vscode.window.showTextDocument(selectedFile);
-            }
-          }
-        }
-        const selectedPath = APP_DIR_PATH.concat("/", selected.name);
-        const selectedUri = vscode.Uri.file(selectedPath);
-        showDirectoryContents(selectedUri);
+        nextInit();
         break;
-
+      case "sveltekit":
+        break;
       default:
         break;
     }
