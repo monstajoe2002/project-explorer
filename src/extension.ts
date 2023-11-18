114f1c986070ebb9d4a91d89a3359e5205eb4e47
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-11-18 15:45:08 +0200

    refactor class into nextjs instead of by command name for example

--- src/extension.ts
+++ src/extension.ts
@@ -30,46 +30,66 @@ class SearchFileCommand implements Command {
     context.subscriptions.push(disposable);
   }
 }
+class NextJsCommand implements Command {
+  name: CommandName;
+  readonly commandId: CommandId;
+  framework: Framework;
+  constructor(name: CommandName) {
+    this.name = name;
+    this.framework = "nextjs";
+    this.commandId = `project-explorer.${this.framework}.${name}`;
+  }
+  isValidNextFile = (fileName: string) =>
+    fileName.endsWith(".tsx") || fileName.endsWith(".jsx");
+
+  register(context: vscode.ExtensionContext, cb: () => void): void {
+    const disposable = vscode.commands.registerCommand(this.commandId, cb);
+    context.subscriptions.push(disposable);
+  }
+}
 const uri = vscode.workspace.workspaceFolders![0].uri;
 const workspacePath = uri.fsPath;
 
 export function activate(_: vscode.ExtensionContext) {
-  const { framework, isValidNextFile, register } = new SearchFileCommand(
-    "nextjs"
-  );
+  const { framework, register, isValidNextFile } = new NextJsCommand("search");
 
   if (!vscode.workspace.workspaceFolders) {
     vscode.window.showInformationMessage("No workspace folder open");
     return;
   }
+
+  register(_, async () => {
+    const workspaceUri = vscode.workspace.workspaceFolders![0].uri;
+    const appDirUri = vscode.Uri.joinPath(workspaceUri, "app");
+    await showDirectoryContents(appDirUri);
+  });
+
   async function showDirectoryContents(uri: vscode.Uri) {
     const filesAndFolders = await vscode.workspace.fs.readDirectory(uri);
-    const folders = filesAndFolders
-      .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
-      .map(([name, fileType]) => ({
-        name,
-        fileType,
-        label: `${name}/`,
-        description: "Folder",
-      }));
-    const files = filesAndFolders
+    const items = filesAndFolders
       .filter(
         ([name, fileType]) =>
-          fileType === vscode.FileType.File && isValidNextFile(name)
+          isValidNextFile(name) || fileType === vscode.FileType.Directory
       )
-      .map(([name, fileType]) => ({
-        name,
-        fileType,
-        label: "/",
-        description: name.includes("page") ? "page" : "layout",
-      }));
-    const selectedItem = await vscode.window.showQuickPick([
-      ...files,
-      ...folders,
-    ]);
+      .map(([name, type]) => ({
+        fileName: name,
+        label: isValidNextFile(name) ? "/" : name,
+        isDirectory: type === vscode.FileType.Directory,
+        description:
+          type === vscode.FileType.Directory
+            ? "folder"
+            : name.includes("page")
+            ? "page"
+            : "layout",
+      }))
+      .sort((a, b) => (a.label === b.label ? 0 : a.label > b.label ? 1 : -1));
+
+    const selectedItem = await vscode.window.showQuickPick(items, {
+      placeHolder: "Select a file or folder",
+    });
     if (selectedItem) {
-      const selectedUri = vscode.Uri.joinPath(uri, selectedItem.name);
-      if (selectedItem.fileType === vscode.FileType.Directory) {
+      const selectedUri = vscode.Uri.joinPath(uri, selectedItem.fileName);
+      if (selectedItem.isDirectory) {
         await showDirectoryContents(selectedUri);
       } else {
         const selectedFile = await vscode.workspace.openTextDocument(
@@ -79,54 +99,6 @@ export function activate(_: vscode.ExtensionContext) {
       }
     }
   }
-  async function nextInit() {
-    const APP_DIR_PATH = `${workspacePath}/app`;
-    const appDirUri = vscode.Uri.file(APP_DIR_PATH);
-    const appDir = await vscode.workspace.fs.readDirectory(appDirUri);
-    if (!appDir) {
-      return;
-    }
-    const appDirFolders = appDir
-      .filter(([_, fileType]) => fileType === vscode.FileType.Directory)
-      .map(([name, fileType]) => ({
-        name,
-        fileType,
-        label: `${name}/`,
-        description: "Folder",
-      }));
-    const appDirFiles = appDir
-      .filter(
-        ([name, fileType]) =>
-          fileType === vscode.FileType.File && isValidNextFile(name)
-      )
-      .map(([name, fileType]) => ({
-        name,
-        fileType,
-        label: "/",
-        description: name.includes("page") ? "page" : "layout",
-      }));
-    const selected = await vscode.window.showQuickPick([
-      ...appDirFiles,
-      ...appDirFolders,
-    ]);
-    if (!selected) {
-      return;
-    }
-    const selectedPath = APP_DIR_PATH.concat("/", selected.name);
-    const selectedUri = vscode.Uri.file(selectedPath);
-    showDirectoryContents(selectedUri);
-  }
-  register(_, async () => {
-    switch (framework) {
-      case "nextjs":
-        nextInit();
-        break;
-      case "sveltekit":
-        break;
-      default:
-        break;
-    }
-  });
 }
 
 // This method is called when your extension is deactivated
