4140288cbe4b06442f4d111f3f5d04f7ca54447c
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-11-22 12:38:50 +0200

    setup tree data provider

---
+++ src/utils/file.ts
@@ -0,0 +1,10 @@
+import * as vscode from "vscode";
+export default class FileTreeItem extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly command?: vscode.Command
+  ) {
+    super(label, collapsibleState);
+  }
+}
