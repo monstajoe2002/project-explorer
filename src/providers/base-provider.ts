20745b5f04723cfdc7e8fff1104bed6f59aa9a30
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-12-12 13:20:51 +0200

    refactor: create base provider class

---
+++ src/providers/base-provider.ts
@@ -0,0 +1,24 @@
+import * as vscode from "vscode";
+import FileTreeItem from "../utils/file-tree-item";
+abstract class Provider {
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    void | FileTreeItem | FileTreeItem[] | null | undefined
+  > = new vscode.EventEmitter<
+    void | FileTreeItem | FileTreeItem[] | null | undefined
+  >();
+  onDidChangeTreeData?:
+    | vscode.Event<void | FileTreeItem | FileTreeItem[] | null | undefined>
+    | undefined = this._onDidChangeTreeData.event;
+  refresh(): void {
+    this._onDidChangeTreeData?.fire();
+  }
+  getTreeItem(
+    element: FileTreeItem
+  ): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
+
+  abstract getChildren(
+    element?: FileTreeItem | undefined
+  ): vscode.ProviderResult<FileTreeItem[]>;
+}
