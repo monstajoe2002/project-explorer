e9e4dc17862813298601081e9528d305adffada2
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-12-12 14:08:58 +0200

    chore: implement active editors provider

--- src/providers/active-editors-provider.ts
+++ src/providers/active-editors-provider.ts
@@ -1,14 +1,59 @@
 import * as vscode from "vscode";
-import FileTreeItem from "../utils/file-tree-item";
 import { Provider } from "../utils/base-provider";
 import ActiveEditorsTreeItem from "../utils/active-editors-tree-item";
 class ActiveEditorsProvider
   extends Provider<ActiveEditorsTreeItem>
-  implements vscode.TreeDataProvider<FileTreeItem>
+  implements vscode.TreeDataProvider<ActiveEditorsTreeItem>
 {
+  constructor(protected _projectDirUri: vscode.Uri) {
+    super(_projectDirUri);
+  }
+  protected readonly _onDidChangeTreeData: vscode.EventEmitter<
+    void | ActiveEditorsTreeItem | ActiveEditorsTreeItem[] | null | undefined
+  > = new vscode.EventEmitter<
+    void | ActiveEditorsTreeItem | ActiveEditorsTreeItem[] | null | undefined
+  >();
+  onDidChangeTreeData?:
+    | vscode.Event<
+        | void
+        | ActiveEditorsTreeItem
+        | ActiveEditorsTreeItem[]
+        | null
+        | undefined
+      >
+    | undefined = this._onDidChangeTreeData.event;
+  refresh(): void {
+    this._onDidChangeTreeData?.fire();
+  }
+  getTreeItem(
+    element: ActiveEditorsTreeItem
+  ): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
   getChildren(
-    element?: FileTreeItem | undefined
-  ): vscode.ProviderResult<FileTreeItem[]> {
-    throw new Error("Method not implemented.");
+    element?: ActiveEditorsTreeItem | undefined
+  ): vscode.ProviderResult<ActiveEditorsTreeItem[]> {
+    if (!element) {
+      return null;
+    }
+
+    vscode.window.visibleTextEditors.map((editor) => {
+      const activeEditor = new ActiveEditorsTreeItem(
+        editor.document.fileName,
+        vscode.TreeItemCollapsibleState.None
+      );
+      return editor.document.fileName.includes("page" || "layout")
+        ? activeEditor
+        : null;
+    });
+  }
+  closeEditor(element: ActiveEditorsTreeItem) {
+    vscode.window.visibleTextEditors.map((editor) => {
+      if (editor.document.fileName === element.label) {
+        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+      }
+    });
+    this.refresh();
+    return;
   }
 }
